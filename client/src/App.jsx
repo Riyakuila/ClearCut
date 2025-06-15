582d4f396abf5b51582cb9c490186eda59f83965
Riyakuila <riyakuila1006@gmail.com>
2025-06-15 09:57:09 +0530

    corrected import result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { ToastContainer, toast } from 'react-toastify';
 import Home from './pages/Home';
-import Result from './pages/result';
+import Result from './pages/Result';
 import BuyCredit from './pages/buyCredit';
 
 
